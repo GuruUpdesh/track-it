b303f03977fa7c27a93d00e273fd773691e93132
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-07-05 16:37:04 -0400

    test prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
