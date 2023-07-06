d4b3b869195e49151993fe016c436ef09ee4dce4
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-07-05 21:43:49 -0400

    card html layout nad component definition

---
+++ components/Card.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <img>
+                </img>
+                <p>Img</p>
+            </div>
+            <div>
+                <h3>
+                    Name
+                </h3>
+                <div>
+                    <p>Icon</p>
+                    <p>Courier</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <p>map</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
