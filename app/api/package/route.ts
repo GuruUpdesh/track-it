04fa848b936fac9f3d1a907e1d1b3d4df5febbc3
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-07-12 12:38:36 -0700

    improved shippo type defintion, added some basic verification to shippo response and simplified returned tracking information returned from api

--- app/api/package/route.ts
+++ app/api/package/route.ts
@@ -1,9 +1,70 @@
 import { NextRequest, NextResponse } from "next/server"
 import axios from "axios"
 
+type TLocation = {
+	city: string
+	state: string
+	zip: string
+	country: string
+}
+
+export type TCourier = "ups" | "usps" | "ontrac" | "dhl" | "fedex"
+
+// ref https://docs.goshippo.com/docs/tracking/tracking/
+export type TStatus =
+	| "PRE_TRANSIT"
+	| "TRANSIT"
+	| "DELIVERED"
+	| "RETURNED"
+	| "FAILURE"
+	| "UNKNOWN"
+
+interface ShippoResponse {
+	carrier: TCourier
+	tracking_number: string
+	address_from: TLocation | null
+	address_to: TLocation | null
+	transaction: string | null
+	original_eta: string | null
+	eta: string | null
+	servicelevel: {
+		token: string | null
+		name: string | null
+	}
+	metadata: string | null
+	tracking_status: {
+		status_date: string
+		status_details: string
+		location: TLocation | null
+		substatus: {
+			code: string | null
+			text: string | null
+			action_required: boolean | null
+		}
+		object_created: string
+		object_updated: string | null
+		object_id: string
+		status: TStatus
+	}
+	tracking_history: ShippoTrackingHistory[]
+}
+
+interface ShippoTrackingHistory {
+	object_created: string
+	object_updated?: string // This field seems to be optional in tracking_history.
+	object_id: string
+	status: TStatus
+	status_details: string
+	status_date: string
+	location: TLocation
+}
+
 const SHIPPO_API_KEY = "ShippoToken " + process.env.SHIPPO_KEY
 
-async function fetchTrackingInfo(trackingNumber: string, courier: string) {
+async function fetchTrackingInfo(
+	trackingNumber: string,
+	courier: TCourier
+): Promise<ShippoResponse> {
 	const { data, status } = await axios.get(
 		`https://api.goshippo.com/tracks/${courier}/${trackingNumber}`,
 		{
@@ -16,7 +77,34 @@ async function fetchTrackingInfo(trackingNumber: string, courier: string) {
 		throw new Error("API call failed")
 	}
 
-	return data
+	// verify that the response is what we expect
+	const responseKeys = Object.keys(data)
+	const expectedKeys = [
+		"carrier",
+		"tracking_number",
+		"address_from",
+		"address_to",
+		"transaction",
+		"original_eta",
+		"eta",
+		"servicelevel",
+		"metadata",
+		"tracking_status",
+		"tracking_history",
+	]
+	const missingKeys = expectedKeys.filter(
+		(key) => !responseKeys.includes(key)
+	)
+	if (missingKeys.length > 0) {
+		throw new Error(`Response is missing keys: ${missingKeys.join(", ")}`)
+	}
+
+	return data as ShippoResponse
+}
+
+function isTCourier(courier: string): courier is TCourier {
+	const couriers: TCourier[] = ["ups", "usps", "ontrac", "dhl", "fedex"]
+	return couriers.includes(courier as TCourier)
 }
 
 export async function GET(request: NextRequest) {
@@ -24,7 +112,7 @@ export async function GET(request: NextRequest) {
 	const trackingNumber = url.searchParams.get("trackingNumber")
 	const courier = url.searchParams.get("courier")
 
-	if (!trackingNumber || !courier) {
+	if (!trackingNumber || !courier || !isTCourier(courier)) {
 		return new NextResponse(
 			JSON.stringify({
 				error: "Missing tracking number or courier in url parameters",
@@ -35,7 +123,21 @@ export async function GET(request: NextRequest) {
 		)
 	}
 
-	const packageAPIInfo = await fetchTrackingInfo(trackingNumber, courier)
+	const packageInfo = await fetchTrackingInfo(trackingNumber, courier)
+	// convert into simpler format
+	const packageInfoSimple = {
+		trackingNumber: packageInfo.tracking_number,
+		courier: packageInfo.carrier,
+		status: packageInfo.tracking_status.status,
+		trackingHistory: packageInfo.tracking_history.map((history) => ({
+			status: history.status,
+			statusDetails: history.status_details,
+			statusDate: history.status_date,
+			location: history.location,
+		})),
+	}
 
-	return new Response(JSON.stringify(packageAPIInfo), { status: 200 })
+	return new Response(JSON.stringify(packageInfoSimple, null, 2), {
+		status: 200,
+	})
 }
