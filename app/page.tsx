79b6b58e536ea8be5de305c61060e307f7bc7065
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-07-13 13:37:14 -0700

    improved responsiveness of breakpoints

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Grid from "@/components/Packages"
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center p-24">
+		<main className="flex min-h-screen flex-col items-center p-10 md:p-18 lg:p-24">
 			<Grid />
 		</main>
 	)
