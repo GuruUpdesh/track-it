3102213b6d1919371df2a134024e1a28f658685d
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-07-13 21:15:58 -0700

    added base route for journey percentage

---
+++ app/api/estimate/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from "next/server"
+import { PackageInfo } from "../package/route"
+
+export async function GET(request: NextRequest) {
+	const url = new URL(request.url)
+	const packageInfoRequested = url.searchParams.get("packageInfo")
+	if (!packageInfoRequested) {
+		return new Response(
+			JSON.stringify({
+				error: "Missing package info in url parameters",
+			}),
+			{
+				status: 400,
+			}
+		)
+	}
+
+	const packageInfo = JSON.parse(packageInfoRequested) as PackageInfo
+
+	console.log(packageInfo)
+}
