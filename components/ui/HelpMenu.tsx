f3eca32344716588058ab8e1b1d0e7115a93f1ce
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-08-07 10:41:41 -0700

    hide help menu on small screen sizes

--- components/ui/HelpMenu.tsx
+++ components/ui/HelpMenu.tsx
@@ -99,7 +99,7 @@ const HelpMenu = () => {
 	]
 	return (
 		<Menu open={open} setOpen={setOpen} menu={menu}>
-			<button className="fixed bottom-2 right-2 z-40 rounded-full border border-indigo-400/50 bg-black p-2 text-xl text-indigo-100 hover:border-indigo-400/75 hover:text-white hover:shadow-sm hover:shadow-indigo-700 sm:bottom-10 sm:right-10">
+			<button className="fixed bottom-2 right-2 z-40 hidden rounded-full border border-indigo-400/50 bg-black p-2 text-xl text-indigo-100 hover:border-indigo-400/75 hover:text-white hover:shadow-sm hover:shadow-indigo-700 sm:bottom-10 sm:right-10 md:block">
 				<BiQuestionMark />
 			</button>
 		</Menu>
