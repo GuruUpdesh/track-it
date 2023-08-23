2ae09ca6b841cb2da6bd35b566d2cd60f8804dbe
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-08-23 12:39:17 -0700

    dnd, theme update, updated skeleton

--- src/app/dashboard/components/shipment/Shipment.tsx
+++ src/app/dashboard/components/shipment/Shipment.tsx
@@ -1,4 +1,7 @@
-import { TShipmentRecord } from "@/app/api/shipment/typesAndSchemas"
+import {
+	TShipmentRecord,
+	shipmentRecordSchema,
+} from "@/app/api/shipment/typesAndSchemas"
 import React from "react"
 import { Card, CardDescription, CardHeader } from "@/components/ui/card"
 import ShipmentMenu from "./ShipmentMenu"
@@ -14,18 +17,29 @@ import { motion } from "framer-motion"
 // 	FiArrowUpLeft,
 // } from "react-icons/fi"
 // import { getTrackingInfo } from "@/app/api/track/trakingAPI"
-// import { cn } from "@/lib/utils"
 // import { TTrackingData } from "@/app/api/package/typesAndSchemas"
 import { useShipments } from "@/lib/shipmentsStore"
+import { cn } from "@/lib/utils"
 
 type Props = {
 	shipmentRecord: TShipmentRecord
 }
 
 const Shipment = ({ shipmentRecord }: Props) => {
-	const [displayedShipments] = useShipments((state) => [
+	const [displayedShipments, updateShipment] = useShipments((state) => [
 		state.displayedShipments,
+		state.updateShipment,
 	])
+
+	interface IDragState {
+		indicator: number | null
+		dragging: boolean
+	}
+
+	const [drag, setDrag] = React.useState<IDragState>({
+		indicator: null,
+		dragging: false,
+	})
 
 	// const [trackingInfo, setTrackingInfo] =
 	// 	React.useState<TTrackingData | null>(null)
@@ -38,6 +52,51 @@ const Shipment = ({ shipmentRecord }: Props) => {
 		// setTrackingInfo(trackingInfoData)
 	}
 
+	function getShipmentFromDataTransfer(dataTransfer: DataTransfer) {
+		const data = dataTransfer.getData("shipment")
+		return shipmentRecordSchema.parse(JSON.parse(data))
+	}
+
+	const dragAndDrop = {
+		onDragStart: (ev: React.DragEvent<HTMLDivElement>) => {
+			ev.dataTransfer.setData("shipment", JSON.stringify(shipmentRecord))
+			setDrag({ ...drag, dragging: true })
+		},
+		onDragOver: (ev: React.DragEvent<HTMLDivElement>) => {
+			ev.preventDefault()
+			const shipment = getShipmentFromDataTransfer(ev.dataTransfer)
+			// if the shipment is the same as the one being dragged set the indicator to 0
+			let indicator = null
+			if (shipment.position === shipmentRecord.position) {
+				indicator = 0
+			} else if (shipment.position < shipmentRecord.position) {
+				indicator = -1
+			} else if (shipment.position > shipmentRecord.position) {
+				indicator = 1
+			}
+			setDrag({ ...drag, indicator })
+		},
+		onDragLeave: (ev: React.DragEvent<HTMLDivElement>) => {
+			ev.preventDefault()
+			setDrag({ ...drag, indicator: null })
+		},
+		onDrop: async (ev: React.DragEvent<HTMLDivElement>) => {
+			ev.preventDefault()
+			const shipment = getShipmentFromDataTransfer(ev.dataTransfer)
+			if (shipment.position === shipmentRecord.position) return
+
+			updateShipment({
+				id: shipment.id,
+				position: shipmentRecord.position,
+			})
+			setDrag({ indicator: null, dragging: false })
+		},
+		onDragEnd: (ev: React.DragEvent<HTMLDivElement>) => {
+			ev.preventDefault()
+			setDrag({ indicator: null, dragging: false })
+		},
+	}
+
 	React.useEffect(() => {
 		fetchTrackingInfo()
 	}, [shipmentRecord.trackingNumber, shipmentRecord.courier])
@@ -48,8 +107,31 @@ const Shipment = ({ shipmentRecord }: Props) => {
 		<motion.div
 			layoutId={`card-${shipmentRecord.id}`}
 			transition={{ duration: 0.3, ease: [0.075, 0.82, 0.165, 1] }}
+			className="relative"
 		>
-			<Card className="group overflow-hidden bg-white/5">
+			<div
+				className={cn(
+					"absolute h-full w-1 scale-y-0 rounded-full bg-[hsl(var(--primary))] opacity-0 transition-transform",
+					{
+						"scale-y-100 opacity-100":
+							drag.indicator && Math.abs(drag.indicator) === 1,
+						"-right-3": drag.indicator === -1,
+						"-left-3": drag.indicator === 1,
+					}
+				)}
+			/>
+			<Card
+				draggable
+				onDragStart={dragAndDrop.onDragStart}
+				onDragOver={dragAndDrop.onDragOver}
+				onDragLeave={dragAndDrop.onDragLeave}
+				onDrop={dragAndDrop.onDrop}
+				onDragEnd={dragAndDrop.onDragEnd}
+				className={cn(
+					"group overflow-hidden bg-white/5 transition-opacity",
+					drag.dragging && "opacity-50"
+				)}
+			>
 				<div className="">
 					<CardHeader className="flex flex-row items-center justify-between px-4 py-2">
 						<div className="flex items-baseline gap-2">
--- src/app/dashboard/components/shipment/ShipmentSkeleton.tsx
+++ src/app/dashboard/components/shipment/ShipmentSkeleton.tsx
@@ -1,24 +1,22 @@
 import React from "react"
-import { Card, CardDescription, CardHeader } from "@/components/ui/card"
+import { Card, CardHeader } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 
 const ShipmentSkeleton = () => {
 	return (
 		<Card className="overflow-hidden bg-white/5">
-			<div className="border-b">
+			<div>
 				<CardHeader className="flex flex-row items-center justify-between px-4 py-2">
 					<div className="flex items-baseline gap-2">
 						<Skeleton className="h-[25px] w-[125px]" />
-						<CardDescription>
-							<Skeleton className="h-[18px] w-[50px]" />
-						</CardDescription>
+						<Skeleton className="h-[18px] w-[50px]" />
 					</div>
 					<div className="flex gap-1">
 						<Skeleton className="h-[40px] w-[40px]" />
 					</div>
 				</CardHeader>
 			</div>
-			<div className="relative min-h-[150px] min-w-full bg-[hsl(var(--background))]"></div>
+			{/* <div className="relative min-h-[150px] min-w-full bg-[hsl(var(--background))]"></div> */}
 		</Card>
 	)
 }
